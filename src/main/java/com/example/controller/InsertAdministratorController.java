41db3d36409d5997d37b1dbcca1eacc891b3a25f
TadunaKitamura <taduna.kitamura0324@gmail.com>
2024-02-09 13:54:16 +0900

    formクラス、Insert.html,AdminControllerで管理者登録画面の表示の実装

--- src/main/java/com/example/controller/InsertAdministratorController.java
+++ src/main/java/com/example/controller/InsertAdministratorController.java
@@ -1,5 +1,27 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
 public class InsertAdministratorController {
-    
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    @GetMapping("/toInsert")
+    public String toInsert(InsertAdministratorForm insertAdministratorForm) {
+
+
+        return "/administrator/insert";
+    }
+
 }

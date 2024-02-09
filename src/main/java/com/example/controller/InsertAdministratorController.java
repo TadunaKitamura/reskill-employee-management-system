197beaa6319af6f96296aa9741ed4b6be1a256dd
TadunaKitamura <taduna.kitamura0324@gmail.com>
2024-02-09 14:21:19 +0900

    管理者登録処理の実装

--- src/main/java/com/example/controller/InsertAdministratorController.java
+++ src/main/java/com/example/controller/InsertAdministratorController.java
@@ -1,14 +1,18 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/")
@@ -20,8 +24,23 @@ public class InsertAdministratorController {
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm insertAdministratorForm) {
 
-
         return "/administrator/insert";
+    }
+
+    @PostMapping("/insert")
+    public String inset(InsertAdministratorForm insertAdministratorForm) {
+
+        Administrator administrator = new Administrator();
+
+        administrator.setId(administrator.getId());
+        administrator.setName(insertAdministratorForm.getName());
+        administrator.setMailAddress(insertAdministratorForm.getMailAddress());
+        administrator.setPassword(insertAdministratorForm.getPassword());
+
+        BeanUtils.copyProperties(insertAdministratorForm, administrator);
+        administratorService.insert(administrator);
+
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,13 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
+import com.example.form.InsertAdministratorForm;
 import com.example.repository.AdministratorRepository;
 
 @Service
 @Transactional
 public class AdministratorService {
-    
+
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者登録の処理
+     * @param administrator
+     */
+    public void insert(Administrator administrator) {
+
+        administratorRepository.insert(administrator);
+    }
 
 }
